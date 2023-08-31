f9e35065049cb6f8f4ad5cff5ae81affec0bc2d5
Elizabethweatherby <elizabethwarren2792@gmail.com>
2023-08-31 12:18:26 -0500

    updated product cascade on delete

--- models/index.js
+++ models/index.js
@@ -5,12 +5,12 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
-    foreignKey: 'category_id'
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
 });
 
 Category.hasMany(Product, {
-    foreignKey: 'category_id',
-    onDelete: 'SET NULL'
+    foreignKey: 'category_id'
 })
 
 Product.belongsToMany(Tag, {
